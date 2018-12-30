c0d6d4ec3083424bd77671747807a6e3f4eeb4ac
Heiko Piirme <heiko.piirme@gmail.com>
2018-12-30 20:37:24 +0200

    Added websockets

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express');
 var mysql= require('mysql');
+var io = require('socket.io').listen(3000);
+var express = require('express');
 var app = express();
 
 var allowCrossDomain = function(req, res, next) {
@@ -27,28 +28,61 @@ connection.connect(function(err) {
     }
 })
 
-app.get('/catresults', function(req, res){
-    var query = "SELECT vote_count FROM votes WHERE vote_name = 'cats'";
-    connection.query(query, (err, rows, fields) => {
-        if(err){
-            console.log("Query failed: " + err);
-            res.sendStatus(500);
-        }
-        res.json(rows[0].vote_count);
-    });
-    
-});
+var socketCount = 0;
+var catScore = 0;
+var dogScore = 0;
+var isInitLoad = false;
 
-app.get('/dogresults', function(req, res){
-    var query = "SELECT vote_count FROM votes WHERE vote_name = 'dogs'";
-    connection.query(query, (err, rows, fields) => {
-        if(err){
-            console.log("Query failed: " + err);
-            res.sendStatus(500);
-        }
-        res.json(rows[0].vote_count);
-    });
-    
-});
+io.sockets.on('connection', function(socket){
+    socketCount++;
 
-app.listen(3000);
+    io.sockets.emit('users connected', socketCount);
+
+    socket.on('disconnect', function() {
+        socketCount--;
+        io.sockets.emit('users connected', socketCount);
+    });
+
+    socket.on('cat vote', function() {
+        connection.query("SELECT vote_count FROM votes WHERE vote_name = 'cats'")
+            .on('result', function(data){
+                catScore = data.vote_count;
+            })
+            .on('end', function(){
+                io.sockets.emit('catvotes', catScore);
+            });
+    })
+
+    socket.on('dog vote', function() {
+        connection.query("SELECT vote_count FROM votes WHERE vote_name = 'dogs'")
+            .on('result', function(data){
+                dogScore = data.vote_count;
+            })
+            .on('end', function(){
+                io.sockets.emit('dogvotes', dogScore);
+            });
+    })
+
+    if (! isInitLoad) {
+        connection.query("SELECT vote_count FROM votes WHERE vote_name = 'cats'")
+            .on('result', function(data){
+                catScore = data.vote_count;
+            })
+            .on('end', function(){
+                socket.emit('catvotes', catScore);
+            });
+
+        connection.query("SELECT vote_count FROM votes WHERE vote_name = 'dogs'")
+            .on('result', function(data){
+                dogScore = data.vote_count;
+            })
+            .on('end', function(){
+                socket.emit('dogvotes', dogScore);
+            });
+ 
+        isInitLoad = true;
+    } else {
+        socket.emit('catvotes', catScore);
+        socket.emit('dogvotes', dogScore);
+    }
+});
